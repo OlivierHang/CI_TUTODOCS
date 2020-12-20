f27f8a38e565e8076aaebea2c27ddd112668958b
Olivier Hang <hang.sengolivier@gmail.com>
2020-12-20 14:07:38 +0100

    Changement de 'baseURL' dans 'App.php' + changement de l'url des liens avec 'base_url()' dans la view 'news/overview'

--- app/Views/news/overview.php
+++ app/Views/news/overview.php
@@ -9,13 +9,7 @@
         <div class="main">
             <?= esc($news_item['body']); ?>
         </div>
-        <p><a href="/news/<?= esc($news_item['slug'], 'url'); ?>">View article</a></p>
-
-    <?php endforeach; ?>
-
-<?php else : ?>
-
-    <h3>No News</h3>
+        <p><a href="<?= base_url() ?>/news/<?= esc($news_item['slug'], 'url'); ?>">View article</a> </p> <?php endforeach; ?> <?php else : ?> <h3>No News</h3>
 
     <p>Unable to find any news for you.</p>
 
